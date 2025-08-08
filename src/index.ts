6fa3d86961f8dd3374f6e07518eccd959cbc2c36
devankitx <126304960+devankitx@users.noreply.github.com>
2025-08-08 21:41:00 +0530

    broadcast chat app working

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ wss.on("connection", (socket) => {
   console.log("User is connected " + userCount);
 
   socket.on("message", (message) => {
-    console.log("message received " + message.toString());
+    console.log(message.toString());
     for (const s of allsockets) {
       if (s) s.send(message.toString() + ": sent from the server");
     }
